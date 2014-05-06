83803a37596117cfadee27f9f4f00e524ba8f1d4
Jeff Taggart <jeff@jetaggart.com>
2014-05-06 16:42:35 -0600

    Made test more robust

--- spec/models/git_hub_api_spec.rb
+++ spec/models/git_hub_api_spec.rb
@@ -4,11 +4,16 @@ describe GitHubApi do
   describe "#comments_for" do
     it "returns all comments based on tracker comments" do
       VCR.use_cassette "github/project/tracker-api-example/commit_comment" do
-        tracker_comment = TrackerComment.new("text" => "Commit by Jeff Taggart https://github.com/jetaggart/tracker-api-example/commit/9bb903237447a032c80ba87cd6437c6769dfcd8c [#70776466] Implement comments on projects")
+        tracker_comments = [TrackerComment.new("text" => "Commit by Jeff Taggart https://github.com/jetaggart/tracker-api-example/commit/9bb903237447a032c80ba87cd6437c6769dfcd8c [#70776466] Implement comments on projects"),
+                            TrackerComment.new("text" => "Commit by Jeff Taggart https://github.com/jetaggart/tracker-api-example/commit/bb670aa13fbd5228871fd1170f047b4e12ccd555 [#70780910] Added tracker comments for all stories")]
 
-        commit_comments = GitHubApi.new.comments_for(:tracker_comments => [tracker_comment])
-        expect(commit_comments.length).to eq(1)
-        expect(commit_comments.first.text).to eq("This is a comment on this commit")
+        commit_comments = GitHubApi.new.comments_for(:tracker_comments => tracker_comments)
+
+        expect(commit_comments.length).to eq(2)
+
+        texts = commit_comments.map(&:text)
+        expect(texts).to include("This is a comment on this commit")
+        expect(texts).to include("This is a second commit comment")
       end
     end
 
