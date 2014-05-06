2754fc9ff0b48f7df381ee50c13ee11b5445e34f
Jeff Taggart <jeff@jetaggart.com>
2014-05-06 16:44:46 -0600

    Formatting

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,10 +5,8 @@ class ProjectsController < ApplicationController
 
   def show
     @stories          = TrackerApi.new.stories(:project_id => params[:id])
-
     @tracker_comments = TrackerApi.new.comments(:project_id => params[:id],
                                                 :story_ids  => @stories.map(&:id))
-
-    @github_comments = GitHubApi.new.comments_for(:tracker_comments => @tracker_comments)
+    @github_comments  = GitHubApi.new.comments_for(:tracker_comments => @tracker_comments)
   end
 end
